b9917d968ccfdb52ed620d0f28069b3db1d95528
artman344 <arthur.manena@gmail.com>
2017-07-18 10:07:10 +0200

    Added project files
    Added first project files

---
+++ MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace MallNavigationApp
+{
+    [Activity(Label = "MallNavigationApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            Button showPopupMenu = FindViewById<Button>(Resource.Id.popupButton);
+            showPopupMenu.Click += (s, arg) => {
+                PopupMenu menu = new PopupMenu(this, showPopupMenu);
+                menu.Inflate(Resource.Menu.popMenu);
+                menu.Show();
+            };
+        }
+    }
+}
+
